7de0b21f69bebb31b4276c5bf150a54e5f681696
Dominic Lewis <dominiclewis@users.noreply.github.com>
2017-01-19 20:22:04 +0000

    Create World.java

---
+++ World.java
@@ -0,0 +1,24 @@
+package zombiestarter; 
+
+/*
+Author: Dominic Lewis
+*/
+
+public class World{
+    //Attributes
+    private String info; 
+    
+    //Constructor
+World(String info){
+    
+    this.info = info;  //this forces class info as opposed to passed one    
+    
+}    
+
+    //Right click inside the class, insert code, getter 
+    public String getInfo() {
+        return info;
+    }
+    
+    
+}
