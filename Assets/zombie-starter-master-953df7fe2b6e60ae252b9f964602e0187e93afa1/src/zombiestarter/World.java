a499fa6b49878ba57a5765e0e40e7a5040de726e
dominiclewis <dominiclewis96@gmail.com>
2017-02-03 18:36:16 +0000

    temp

--- Assets/zombie-starter-master-953df7fe2b6e60ae252b9f964602e0187e93afa1/src/zombiestarter/World.java
+++ Assets/zombie-starter-master-953df7fe2b6e60ae252b9f964602e0187e93afa1/src/zombiestarter/World.java
@@ -13,6 +13,8 @@ public class World{   //Private Variables Public class so anything can use the g
     private String displayInventory; //HTML used for formatting the inventory?
     private String startStringHTML; //String to be displayed to the user at the start of the game
     private String start; //String that is used for the start room I think 
+    private String currentRoom;
+    private boolean startRan = false; 
     //Constructor (THIS IS THE INFO WE USE TO CONSTRUCT THE CLASS * CALLTIME)
 World(String info, String displayInventory, String startString,String start){
     
@@ -56,5 +58,34 @@ public String getDisplayInventory(){
         return start; 
         
     }
-     
+    public boolean getStartRan(){
+        return startRan;
+    }
+    
+    public void setStartRan(boolean startRan){
+        this.startRan = startRan;
+ 
+    }
+    
+    public String getCurrentRoom(){
+        
+       return currentRoom;
+    }
+    
+    public void setCurrentRoom(String currentRoom)
+    {
+        this.currentRoom = currentRoom;
+    }
+    //not static so it references the current state of whatever the object/instances start ran is 
+    public void setCurrentRoom(){
+       //if this is the first time we are setting the current room 
+       //It is implicit that we are in the beginner room
+      if(getStartRan() == false)
+      {
+          //May need a try/catch if it breaks 
+       currentRoom = getStart(); //set the current room to be the start string  
+         
+      }
+    }
+    
 }
